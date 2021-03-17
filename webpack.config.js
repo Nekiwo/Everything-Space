d78867d69b020506ca489d640941ad49c2cab849
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-03-17 17:54:58 -0500

    still trying to bundle

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,22 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/ts/index.ts",
+  node: {
+    __dirname: false,
+  },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
+        use: "ts-loader"
+      }
     ],
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: "bundle.js",
+    filename: "index.js",
     path: path.resolve(__dirname, "build"),
   },
 };
