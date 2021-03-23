4ef0d31790b49ed4611cffef918ef2412d6e177f
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-03-22 23:08:32 -0500

    new zoom feature

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -6,18 +6,30 @@ import {OrbitControls} from "three-orbitcontrols-ts";
 
 import {Planet} from "./classes/Planet";
 
-import {DrawPath} from "./functions/DrawPath";
 import {CalcTime} from "./functions/CalcTime";
 
 import {planets} from "./data/Planets";
+
+// Global variables
+
+// Level of graphics from 1-3, changed based on the device's performance
+export var GraphicsLevel: number = 1;
+
 
 // Making a three js scene
 export const scene:THREE.Scene = new THREE.Scene();
 const camera:THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer:THREE.WebGLRenderer = new THREE.WebGLRenderer();
+const renderer:THREE.WebGLRenderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+// Fix canvas size on window resize
+window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
 
 planets.forEach((planet: Planet) => {
     console.log(planet.name);
@@ -28,13 +40,26 @@ const controls: OrbitControls = new OrbitControls(camera, renderer.domElement);
 camera.position.set(0, 0, 0);
 controls.update();
 
-document.addEventListener( 'mousewheel', (event: WheelEvent) => {
-    camera.position.z += event.deltaY;
-    console.log(event.deltaY);
-    //camera.fov = Math.max( Math.min( camera.fov, fovMAX ), fovMIN );
-    //camera.projectionMatrix = new THREE.Matrix4().makePerspective(camera.fov, window.innerWidth / window.innerHeight, camera.near, camera.far);
+
+// Listen for mouse wheel movement for zooming
+// adapt for mobile pinching later: https://stackoverflow.com/a/11183333/12848536
+var CamZoom: number = 4;
+var FocusPos: THREE.Vector3 = new THREE.Vector3();
+
+camera.position.copy(FocusPos);
+camera.translateZ(CamZoom);
+
+document.addEventListener("mousewheel", (event: WheelEvent) => {
+    if (CamZoom > 4) {
+        camera.position.copy(FocusPos);
+        CamZoom += event.deltaY / 25;
+        camera.translateZ(CamZoom);
+    } else {
+        CamZoom = 8;
+    }
 });
 
+// Running every frame, standard three.js/ts
 const animate = function () {
     requestAnimationFrame(animate);
 
