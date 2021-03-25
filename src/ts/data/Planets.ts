bb61d7d6e3345e8c57d76141f3d29365c3a702d3
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-03-24 22:57:36 -0500

    new info

--- src/ts/data/Planets.ts
+++ src/ts/data/Planets.ts
@@ -7,123 +7,123 @@ import {Planet} from "../classes/Planet";
 export var planets:Planet[] = [
     new Planet({
         name: "Sun",
-        radius: 696340000,
+        radius: 0.000696340000,
         mass: 1988500,
         model: "",
         SunDeg: 0,
         XRadius: 0,
         ZRadius: 0,
+        aphelion: 0,
         XTilt: 0,
         ZTilt: 0,
-        apoapsis: 0,
-        inclination: 0
+        LocalTilt: 0
     }),
     new Planet({
         name: "Mercury",
-        radius: 2439,
+        radius: 0.000000002439,
         mass: 0.33,
         model: "",
         SunDeg: 0,
-        XRadius: 0.046,
-        ZRadius: 0.069,
-        XTilt: 0,
+        XRadius: 0.115,
+        ZRadius: 0.056,
+        aphelion: 0.069,
+        XTilt: 7,
         ZTilt: 48.3,
-        apoapsis: 0,
-        inclination: 7
+        LocalTilt: 29.1
     }),
     new Planet({
         name: "Venus",
-        radius: 6502,
+        radius: 0.000000006502,
         mass: 4.87,
         model: "",
         SunDeg: 0,
-        XRadius: 0.107,
+        XRadius: 0.216,
         ZRadius: 0.108,
-        XTilt: 0,
+        aphelion: 0.108,
+        XTilt: 3.4,
         ZTilt: 76.6,
-        apoapsis: 0,
-        inclination: 3.4
+        LocalTilt: 54.8
     }),
     new Planet({
         name: "Earth",
-        radius: 6378,
+        radius: 0.000000006378,
         mass: 5.97,
         model: "",
         SunDeg: 0,
-        XRadius: 0.147,
-        ZRadius: 0.152,
+        XRadius: 0.299,
+        ZRadius: 0.149,
+        aphelion: 0.152,
         XTilt: 0,
         ZTilt: -11.2,
-        apoapsis: 0,
-        inclination: 0
+        LocalTilt: 114.2
     }),
     new Planet({
         name: "Mars",
-        radius: 3396,
+        radius: 0.000000003396,
         mass: 0.642,
         model: "",
         SunDeg: 0,
-        XRadius: 0.206,
-        ZRadius: 0.249,
-        XTilt: 0,
+        XRadius: 0.455,
+        ZRadius: 0.226,
+        aphelion: 0.249,
+        XTilt: 1.9,
         ZTilt: 49.5,
-        apoapsis: 0,
-        inclination: 1.9
+        LocalTilt: 286.5
     }),
     new Planet({
         name: "Jupiter",
-        radius: 71492,
+        radius: 0.000000071492,
         mass: 1898,
         model: "",
         SunDeg: 0,
-        XRadius: 0.740,
-        ZRadius: 0.816,
-        XTilt: 0,
+        XRadius: 1.557,
+        ZRadius: 0.777,
+        aphelion: 0.816,
+        XTilt: 1.3,
         ZTilt: 100.4,
-        apoapsis: 0,
-        inclination: 1.3
+        LocalTilt: 273.8
     }),
     new Planet({
         name: "Saturn",
-        radius: 60268,
+        radius: 0.000000060268,
         mass: 568,
         model: "",
         SunDeg: 0,
-        XRadius: 1.352,
-        ZRadius: 1.514,
-        XTilt: 0,
+        XRadius: 2.867,
+        ZRadius: 1.431,
+        aphelion: 1.514,
+        XTilt: 2.5,
         ZTilt: 113.6,
-        apoapsis: 0,
-        inclination: 2.5
+        LocalTilt: 339.3
     }),
     new Planet({
         name: "Uranus",
-        radius: 25559,
+        radius: 0.000000025559,
         mass: 86.8,
         model: "",
         SunDeg: 0,
-        XRadius: 2.741,
-        ZRadius: 3.003,
-        XTilt: 0,
+        XRadius: 5.744,
+        ZRadius: 2.869,
+        aphelion: 3.003,
+        XTilt: 0.8,
         ZTilt: 74,
-        apoapsis: 0,
-        inclination: 0.8
+        LocalTilt: 96.9
     }),
     new Planet({
         name: "Neptune",
-        radius: 24764,
+        radius: 0.000000024764,
         mass: 102,
         model: "",
         SunDeg: 0,
-        XRadius: 4.444,
-        ZRadius: 4.545,
-        XTilt: 0,
+        XRadius: 8.990,
+        ZRadius: 4.494,
+        aphelion: 4.545,
+        XTilt: 1.8,
         ZTilt: 131.7,
-        apoapsis: 0,
-        inclination: 1.8
+        LocalTilt: 276.3
     })
 ];
-// SOURCE: https://nssdc.gsfc.nasa.gov/pn lanetary/factsheet/
+// SOURCE: https://nssdc.gsfc.nasa.gov/planetary/factsheet/
 // https://en.wikipedia.org/wiki/Longitude_of_the_ascending_node
 // https://en.wikipedia.org/wiki/Argument_of_periapsis
 // https://solarsystem.nasa.gov/planets/overview/
