b271320da6a80e25f12e1cec8b571b3ba23029db
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-03-24 22:57:29 -0500

    new info and render

--- src/ts/classes/Planet.ts
+++ src/ts/classes/Planet.ts
@@ -16,12 +16,13 @@ export class Planet {
     SunDeg: number;
     x: number;
     z: number;
-    XRadius: number; // XRadius of ellipse, AKA Perihelion of the orbit
-    ZRadius: number; // ZRadius of ellipse, AKA Aphelion of the orbit
-    XTilt: number;
-    ZTilt: number;
-    apoapsis: number;
+    aphelion: number;
+    XTilt: number; // XTilt is longitude of the ascending node in this context
+    ZTilt: number; // ZTilt is inclination in this context
+    LocalTilt: number; // LocalTilt is argument of periapsis in this context
     inclination: number;
+    XRadius: number;
+    ZRadius: number;
     constructor(args?: any) {
         this.name = args.name;
         this.radius = args.radius;
@@ -30,15 +31,16 @@ export class Planet {
         this.SunDeg = args.SunDeg;
         this.XRadius = args.XRadius;
         this.ZRadius = args.ZRadius;
-        this.XTilt = args.XTilt; // XTilt is argument of periapsis in this context
-        this.ZTilt = args.ZTilt; // ZTilt is longitude of the ascending node in this context
-        this.apoapsis = args.apoapsis;
+        this.aphelion = args.aphelion;
+        this.XTilt = args.XTilt;
+        this.ZTilt = args.ZTilt;
+        this.LocalTilt = args.LocalTilt;
         this.inclination = args.inclination;
         // Calculate the coords for the ellipse instead of circle
         
         
         this.x = this.XRadius * Math.cos((this.SunDeg - 90) * (Math.PI/180));
-        this.z = this.ZRadius * Math.sin((this.SunDeg - 90) * (Math.PI/180));
+        this.z = this.aphelion * Math.sin((this.SunDeg - 90) * (Math.PI/180));
     }
     DrawPlanet: Function = (SunDeg: number = this.SunDeg, scene: THREE.Scene) => {
         this.SunDeg = SunDeg;
@@ -49,7 +51,7 @@ export class Planet {
 
         // Calculate the coords for the ellipse instead of circle and draw new
         var NewX: number = this.XRadius * Math.cos((SunDeg - 90) * (Math.PI/180));
-        var NewZ: number = this.ZRadius * Math.sin((SunDeg - 90) * (Math.PI/180));
+        var NewZ: number = this.aphelion * Math.sin((SunDeg - 90) * (Math.PI/180));
         this.x = NewX;
         this.z = NewZ;
 
@@ -59,7 +61,7 @@ export class Planet {
         var planet:CanvasRenderingContext2D = canvas.getContext("2d");
         var circum:CanvasRenderingContext2D = canvas.getContext("2d");
         circum.beginPath();
-        circum.ellipse(1500, 1500, this.XRadius, this.ZRadius, 0, 0, 2 * Math.PI);
+        circum.ellipse(1500, 1500, this.XRadius, this.aphelion, 0, 0, 2 * Math.PI);
         circum.stroke();
 
         planet.fillStyle = "#ff0000";
@@ -68,14 +70,26 @@ export class Planet {
         planet.fill();
         */
 
-        var geometry: THREE.TorusGeometry = new THREE.TorusGeometry(this.XRadius * 24, 0.05, 6, (GraphicsLevel + 1) * 12);
-        var material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({color: 0xFFFFFF, side: THREE.FrontSide, transparent: true, opacity: 0.3});
-        var circum: THREE.Mesh = new THREE.Mesh(geometry, material);
+        var CircumGeometry: THREE.TorusGeometry = new THREE.TorusGeometry(this.XRadius * 24, 0.05, 6, (GraphicsLevel + 1) * 12);
+        var CircumMaterial: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({color: 0xFFFFFF, side: THREE.FrontSide, transparent: true, opacity: 0.3});
+        var circum: THREE.Mesh = new THREE.Mesh(CircumGeometry, CircumMaterial);
 
-        circum.scale.set(1, this.ZRadius / this.XRadius, 1);
-        circum.rotation.set(this.XTilt * (Math.PI / 180), 0, this.ZTilt * (Math.PI / 180));
+        // I might've messed up here, do a pull request or sumbit an issue on the GitHub repo if you know the right way to rotate the orbits
+        circum.scale.set(1, this.ZRadius * 2 / this.XRadius, 1);
+        circum.rotation.set(this.XTilt * (Math.PI / 180), 0, this.LocalTilt * (Math.PI / 180));
+        circum.rotateOnWorldAxis(new THREE.Vector3(0, 0, 1), this.ZTilt * (Math.PI / 180));
+        circum.translateZ(this.XRadius - this.aphelion);
+        console.log(this.XRadius - this.aphelion);
+    
+        // Testong Planets, models coming soon
+        var PlanetGeometry: THREE.SphereGeometry = new THREE.SphereGeometry(this.radius * 24, 20, 16);
+        var PlanetMaterial: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({color: 0x0000FF, side: THREE.FrontSide});
+        var planet: THREE.Mesh = new THREE.Mesh(PlanetGeometry, PlanetMaterial);
+
+        planet.position.set(0, 0, 0);
 
         scene.add(circum);
+        scene.add(planet);
     }
     RemovePlanet: Function = () => {
         // remove planet from the scene
