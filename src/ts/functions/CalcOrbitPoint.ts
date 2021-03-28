742dfa8eab4e5497e8c25762a565f3671000f50e
Nekiwo <geo.panchev@gmail.com>
2021-03-28 13:13:56 -0500

    starting variables

--- src/ts/functions/CalcOrbitPoint.ts
+++ src/ts/functions/CalcOrbitPoint.ts
@@ -1,4 +1,18 @@
 // Calculate a point of certain angle on an orbit/ellipse using planet data
-export var CalcOrbitPoint: Function = (planet: number) => {
-
+export var CalcOrbitPoint: Function = (args: any) => {
+    // calculate x axis view of new ellipse
+    // calculate y axis view of new ellipse
+    var ZViewXRadius = 0;
+    var ZViewYRadius = 0;
+    var ZViewZAngle = 0;
+    var x = 0;
+    var y = 0;
+    // calculate z axis view of new ellipse
+    var XViewXRadius = 0;
+    var XViewYRadius = 0;
+    var XViewXAngle = 0;
+    var z = 0;
+    // calculate a point on x,y,z views
+    // return result
+    return [x, y, z]
 }
