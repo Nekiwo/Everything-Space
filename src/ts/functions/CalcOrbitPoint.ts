9b3cb77d642a03d04bf64fe13071ca63dd994924
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-04-10 18:21:28 -0500

    finally planet render done

--- src/ts/functions/CalcOrbitPoint.ts
+++ src/ts/functions/CalcOrbitPoint.ts
@@ -1,19 +1,15 @@
-import {GraphicsLevel} from "../index";
 // Calculate a point of certain angle on an orbit/ellipse using planet data
 export var CalcOrbitPoint: Function = (args: any) => {
-    var PreX = args.OrbitRadius * 24 * Math.cos((args.NewSunDeg + args.ZTilt) * (Math.PI / 180));
-    var PreY = args.OrbitRadius * 24 * Math.sin((args.NewSunDeg + args.ZTilt) * (Math.PI / 180));
+    let x = args.OrbitRadius * 24 * Math.cos(args.NewPlanetRotation * (Math.PI/180));
+    let y = args.OrbitRadius * 24 * Math.sin(args.NewPlanetRotation * (Math.PI/180));
 
-    var x = (Math.sqrt(PreX*PreX + PreY*PreY)) * Math.cos(-args.ZTilt * (Math.PI / 180));
-    var y = (Math.sqrt(PreX*PreX + PreY*PreY)) * Math.sin(-args.ZTilt * (Math.PI / 180));
+    // Make Z depend on orbit tilts
+    var AltX = args.OrbitRadius * 24 * Math.cos((args.NewPlanetRotation + args.ZTilt) * (Math.PI/180));
+    var PreZ = args.OrbitRadius * 24 * Math.sin(args.XTilt * (Math.PI/180));
 
-    var XViewXRadius = 0;
-    var XViewYRadius = 0;
-    var XViewXAngle = 0;
-    var z = 0;
+    let z = AltX * (PreZ / (args.OrbitRadius * 24));
 
     // calculate a point on x,y,z views
     // return result
     return [x, y, z]
 }
-//https://math.stackexchange.com/questions/426150/what-is-the-general-equation-of-the-ellipse-that-is-not-in-the-origin-and-rotate
