e1ccad7fd561bbb5c116b0e7dd18042ab67c1e2e
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-04-21 23:06:15 -0500

    new transition fx function

--- src/ts/functions/fx/FadeTransition.ts
+++ src/ts/functions/fx/FadeTransition.ts
@@ -1,4 +1,19 @@
 // Fade transition for scenes
-export var FadeTransition: Function = (planet: number) => {
-
+export var FadeTransition: Function = (type: boolean, seconds: number) => {
+    const screen: CSSStyleDeclaration = document.getElementById("screen").style;
+    /*  Types:
+            true: IN
+            false: OUT
+    */
+    for (let i = 0; i < 1; i += 0.01) {
+        if (type) {
+            setTimeout(() => {
+                screen.opacity = "" + i;
+            }, (Math.cos(Math.PI * i + Math.PI) + 1) / 2 * seconds);
+        } else {
+            setTimeout(() => {
+                screen.opacity = "" + i;
+            }, (Math.cos(Math.PI * i) + 1) / 2 * seconds);
+        }
+    }
 }
