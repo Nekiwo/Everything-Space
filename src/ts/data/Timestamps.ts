f519697298a7e8282828a99d0c1b21bb33e24c2e
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-04-22 22:55:37 -0500

    array for testing

---
+++ src/ts/data/Timestamps.ts
@@ -0,0 +1,68 @@
+// All timestamps
+import {Timestamp} from "../classes/Timestamp";
+export var Timestamps:Timestamp[] = [
+    new Timestamp({
+        name: "Big Bang",
+        year: "13.8B years ago",
+        start: () => {
+
+        },
+        order: 0
+    }),
+    new Timestamp({
+        name: "Big Bang",
+        year: "13.8B years ago",
+        start: () => {
+
+        },
+        order: 1
+    }),
+    new Timestamp({
+        name: "Big Bang",
+        year: "13.8B years ago",
+        start: () => {
+
+        },
+        order: 2
+    }),
+    new Timestamp({
+        name: "Big Bang",
+        year: "13.8B years ago",
+        start: () => {
+
+        },
+        order: 3
+    }),
+    new Timestamp({
+        name: "Big Bang",
+        year: "13.8B years ago",
+        start: () => {
+
+        },
+        order: 4
+    }),
+    new Timestamp({
+        name: "Big Bang",
+        year: "13.8B years ago",
+        start: () => {
+
+        },
+        order: 5
+    }),
+    new Timestamp({
+        name: "Big Bang",
+        year: "13.8B years ago",
+        start: () => {
+
+        },
+        order: 6
+    }),
+    new Timestamp({
+        name: "Big Bang",
+        year: "13.8B years ago",
+        start: () => {
+
+        },
+        order: 7
+    }),
+];
