c72aa6e8b902aeb6b2c9c8757a3f0af3ec4e5461
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-04-22 22:55:22 -0500

    finished class

--- src/ts/classes/Timestamp.ts
+++ src/ts/classes/Timestamp.ts
@@ -3,8 +3,15 @@ import * as THREE from "three";
 // Timestampcene class for each timestamp
 export class Timestamp {
     name: string;
-    order: number;
+    year: number;
     start: Function = async (): Promise<void> => {
         Promise.resolve("test")
     };
+    order: number;
+    constructor(args?: any) {
+        this.name = args.name;
+        this.year = args.year;
+        this.start = args.start;
+        this.order = args.order;
+    }
 }
