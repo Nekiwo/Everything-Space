81069d8281011cdef43f9ae8c1bb6613e4bbc7a4
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-04-26 23:07:44 -0500

    starting work on 1st scened

--- src/ts/data/Timestamps.ts
+++ src/ts/data/Timestamps.ts
@@ -1,68 +1,19 @@
+import {FadeTransition} from "../functions/fx/FadeTransition";
+
+import {BigBang} from "./scenes/BigBang";
+
 // All timestamps
 import {Timestamp} from "../classes/Timestamp";
-export var Timestamps:Timestamp[] = [
+export var Timestamps: Timestamp[] = [
     new Timestamp({
         name: "Big Bang",
         year: "13.8B years ago",
         start: () => {
-
+            console.log("start")
+            document.body.innerHTML = "";
+            FadeTransition(true, 5000);
+            BigBang();
         },
         order: 0
-    }),
-    new Timestamp({
-        name: "Big Bang",
-        year: "13.8B years ago",
-        start: () => {
-
-        },
-        order: 1
-    }),
-    new Timestamp({
-        name: "Big Bang",
-        year: "13.8B years ago",
-        start: () => {
-
-        },
-        order: 2
-    }),
-    new Timestamp({
-        name: "Big Bang",
-        year: "13.8B years ago",
-        start: () => {
-
-        },
-        order: 3
-    }),
-    new Timestamp({
-        name: "Big Bang",
-        year: "13.8B years ago",
-        start: () => {
-
-        },
-        order: 4
-    }),
-    new Timestamp({
-        name: "Big Bang",
-        year: "13.8B years ago",
-        start: () => {
-
-        },
-        order: 5
-    }),
-    new Timestamp({
-        name: "Big Bang",
-        year: "13.8B years ago",
-        start: () => {
-
-        },
-        order: 6
-    }),
-    new Timestamp({
-        name: "Big Bang",
-        year: "13.8B years ago",
-        start: () => {
-
-        },
-        order: 7
-    }),
+    })
 ];
---
+++ src/ts/data/scenes/BigBang.ts
@@ -0,0 +1,68 @@
+import * as THREE from "three";
+import {EffectComposer} from "three/examples/jsm/postprocessing/EffectComposer.js";
+import {RenderPass} from "three/examples/jsm/postprocessing/RenderPass.js";
+import {ShaderPass} from "three/examples/jsm/postprocessing/ShaderPass.js";
+import {UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
+import {FXAAShader} from "three/examples/jsm/shaders/FXAAShader.js";
+import {OrbitControls} from "three-orbitcontrols-ts";
+
+
+// Big Bang scene
+export var BigBang: Function = () => {
+
+
+    // Making a three js scene
+    const scene: THREE.Scene = new THREE.Scene();
+    const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    
+    
+    // Effects setup
+    const RenderScene: RenderPass = new RenderPass(scene, camera);
+    
+    const FxaaPass: ShaderPass = new ShaderPass(FXAAShader);
+    
+    const pixelRatio = renderer.getPixelRatio();
+    
+    FxaaPass.material.uniforms["resolution"].value.x = 1 / (document.body.offsetWidth * pixelRatio);
+    FxaaPass.material.uniforms["resolution"].value.y = 1 / (document.body.offsetHeight * pixelRatio);
+    
+    const BloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85);
+    BloomPass.threshold = 0;
+    BloomPass.strength = 0.9;
+    BloomPass.radius = 0.1;
+    
+    const composer: EffectComposer = new EffectComposer(renderer);
+    composer.addPass(RenderScene);
+    composer.addPass(BloomPass);
+    composer.addPass(FxaaPass);
+
+    // Other
+    window.addEventListener("resize", () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    });
+    
+    const controls: OrbitControls = new OrbitControls(camera, renderer.domElement);
+    camera.position.set(0, 0, 0);
+    controls.update();
+    
+    // Running every frame, standard three.js/ts
+    const animate = function () {
+        requestAnimationFrame(animate);
+    
+        
+        
+        controls.update();
+        composer.render();
+    };
+    
+    animate();
+}
