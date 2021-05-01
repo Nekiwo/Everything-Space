8c91e034cbb1bcef8b3ce85c00db6c8abf686486
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-05-01 18:16:21 -0500

    rename

---
+++ src/ts/classes/NextButton.ts
@@ -0,0 +1,20 @@
+// Next button class for scenes
+export class NextButton {
+    funct: Function[]; // Array of the functions that happen each time the button is pressed, all in a sequential order
+    constructor(funct: Function[]) {
+        this.funct = funct;
+
+        // Index of functions in the array
+        var FunctIndex: number = 0;
+
+        // Initiate the button
+        var NextBtn: HTMLElement = document.createElement("div");
+        NextBtn.innerHTML = "Next";
+        NextBtn.id = "nextbtn";
+
+        NextBtn.onclick = () => {
+            this.funct[FunctIndex]();
+            FunctIndex++;
+        }
+    }
+}
