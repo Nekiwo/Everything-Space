bd5fac2cd4e1428684192fd1cbddde24014e2300
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-05-02 18:16:15 -0500

    class finished

--- src/ts/classes/NextButton.ts
+++ src/ts/classes/NextButton.ts
@@ -2,19 +2,29 @@
 export class NextButton {
     funct: Function[]; // Array of the functions that happen each time the button is pressed, all in a sequential order
     constructor(funct: Function[]) {
+        // Initiate subtitle box
+        var subtitle: HTMLElement = document.createElement("div");
+        subtitle.id = "subtitle";
+        document.body.appendChild(subtitle);
+
         this.funct = funct;
 
         // Index of functions in the array
         var FunctIndex: number = 0;
 
+        // Initiate the first starting function
+        this.funct[FunctIndex]();
+        FunctIndex++;
+
         // Initiate the button
         var NextBtn: HTMLElement = document.createElement("div");
         NextBtn.innerHTML = "Next";
         NextBtn.id = "nextbtn";
-
         NextBtn.onclick = () => {
+            // Add transition later
             this.funct[FunctIndex]();
             FunctIndex++;
         }
+        document.body.appendChild(NextBtn);
     }
 }
