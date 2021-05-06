d04c0025c65cd96781dc8d523cbf1bb885599f80
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-05-05 23:13:54 -0500

    i18n starting

---
+++ src/ts/data/i18n/english.ts
@@ -0,0 +1,20 @@
+export var english: any = {
+    menu: {
+        
+    },
+    subs: {
+        BigBang: {
+            meta : {
+
+            },
+            parts: {
+                1: "The universe as we know it today did not exist over 13.8 billion years ago. Everything existed and didn't exist at the same time.",
+                2: "Until one moment, driven by the dark matter, the universe started expanding.",
+                3: "Very quickly, faster than light.",
+                4: "Just one millisecond later, the first hydrogen nuclei started to form. That seems fast, but the first atoms didn't form until 379,000 years after Big Bang when they finally cooled down to bond with electrons.",
+                5: "After that, the universe continued to expand. First stars were forming at 100 million years in.",
+                6: "Billions of years have passed, forming the place we know today. Even now we recieve a constant background radiation, left as a legacy from Big Bang."
+            }
+        }
+    }
+}
