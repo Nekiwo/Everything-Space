eae043c116e01d3cac9f8c84a6b6e3067e216e05
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-05-06 18:11:41 -0500

    optimization + new callback feature

--- src/ts/functions/fx/FadeTransition.ts
+++ src/ts/functions/fx/FadeTransition.ts
@@ -1,19 +1,23 @@
 // Fade transition for scenes
-export var FadeTransition: Function = (type: boolean, seconds: number) => {
+export var FadeTransition: Function = (type: boolean, seconds: number, callback?: Function) => {
     const screen: CSSStyleDeclaration = document.getElementById("screen").style;
     /*  Types:
             true: IN
             false: OUT
     */
+    // Optimizations https://cdn.discordapp.com/emojis/830135416744050708.png
+    var TypeNumber: number = 0;
+    if (type) {
+        TypeNumber = Math.PI;
+    }
+
     for (let i = 0; i < 1; i += 0.01) {
-        if (type) {
-            setTimeout(() => {
-                screen.opacity = "" + i;
-            }, (Math.cos(Math.PI * i + Math.PI) + 1) / 2 * seconds);
-        } else {
-            setTimeout(() => {
-                screen.opacity = "" + i;
-            }, (Math.cos(Math.PI * i) + 1) / 2 * seconds);
-        }
+        setTimeout(() => {
+            screen.opacity = "" + i;
+        }, (Math.cos(Math.PI * i + TypeNumber) + 1) / 2 * seconds);
+    }
+
+    if (callback !== undefined) {
+        setTimeout(callback(), seconds);
     }
 }
